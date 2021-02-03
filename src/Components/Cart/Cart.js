3613183bd2585efcf18e2df803ad98a7036bef89
ashrafulislampro <ashrafulislam1659@gmail.com>
2021-02-03 08:57:46 -0800

    done online courses

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,12 +4,15 @@ const Cart = (props) => {
     const cart = props.cart;
     const Total = cart.reduce((total,TotalPrice) => total + TotalPrice.price, 0);
     const grandTotal = (Total).toFixed(2);
+    const handleAlert = () => {
+        alert("Morobbi akto jiraia lon");
+    }
     return (
         <div className="cart-text">
             <h3>SUMMARY OF CART</h3>
             <h5>Total Count : {cart.length}</h5>
             <h5>Total Amount : {grandTotal}</h5>
-            <button className="btn btn-primary">Review Cart</button>
+            <button onClick={handleAlert} className="btn btn-primary">Review Cart</button>
         </div>
     );
 };
